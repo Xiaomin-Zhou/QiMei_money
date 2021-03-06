716774906875f5b0dbae0db7f064386ab39e88aa
zxm <zhouxiaomin01@126.com>
2021-03-06 22:21:39 +0800

    可以显示 tag的名字

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -6,10 +6,10 @@
             <span class="rightIcon"></span>  
         </div>
         <div class="form-wrapper">
-            <FormItem field-name="标签名" placeholder="输入标签名"/>
+            <FormItem :value="tag.name" field-name="标签名" placeholder="输入标签名"/>
         </div>
         <div class="button-wrapper">
-            <Button @click="remove">删除标签</Button>
+            <Button>删除标签</Button>
         </div>
     </Layout>
 </template>
