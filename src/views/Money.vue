2aeb81f877878eab07e3101df8eb00831ade9596
zxm <zhouxiaomin01@126.com>
2021-03-08 18:46:30 +0800

    解决Tag.vue 的bug (新增标签正常保存、显示)

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -6,7 +6,7 @@
                 placeholder="在这里输入备注" 
                 @update:value="onUpdateNotes"/>
         </div>
-        <Tags :data-source.sync="tags" @update:value="onUpdateTags"/>
+        <Tags/>
         <Types :value.sync="record.type"/>
     </Layout>
 </template>
@@ -24,16 +24,12 @@
     components: { Tags,FormItem,Types,NumberPad}
     })
     export default class Money extends Vue{
-        tags = store.tagList;
+
         recordList = store.recordList;
         record: RecordItem = {
             tags: [], notes: '', type: '-', amount: 0
-        };
-
-
-        onUpdateTags(value: string[]) {
-            this.record.tags = value;   
         }
+
         onUpdateNotes(value: string) {
             this.record.notes = value;
         }
